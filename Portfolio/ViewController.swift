06e3f48b7a6b0cec3b3bd73705e384222c56f3af
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-09-27 08:30:28 -0500

    Initial Commit

---
+++ Portfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Matthew Riley on 9/27/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

ae1e45e3e5650131f157b917324546a1c77aa7c5
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-12-14 11:01:58 -0600

    1. Final commit

--- Portfolio/ViewController.swift
+++ Portfolio/ViewController.swift
@@ -18,3 +18,13 @@ class ViewController: UIViewController {
 
 }
 
+/*
+INSTRUCTIONS
+1. Make sure to figure out how to add in a picture of yourself
+ 
+2. Add in text describing yourself and your skills. (Think of resume-type information)
+3. Customize the app using colors and constraints to make it look good/professional.
+4. Use different UI elements and challenge yourself to show off the skills that you have learned so far. This is something that you can build on as we continue to learn new skills, and will be a great thing to show off to potential employers!
+*/
+
+
